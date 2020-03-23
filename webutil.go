f26719ea36345e539c92d189c646b5354cd23801
WSW <shengwei.wan@ceasia.com.cn>
2020-03-23 16:26:11 +0800

    update

--- webutil.go
+++ webutil.go
@@ -35,57 +35,36 @@ func HttpJsonRespond(w http.ResponseWriter,json *simplejson.Json ,httpcode int,e
 	w.Write(b)
 }
 
-func HttpJsonStrRespond(w http.ResponseWriter,rootstr string ,httpcode int,errcode int32,errmsg string){
-	sjson.Set(rootstr,"code",httpcode)
+func HttpJsonStrRespond(w http.ResponseWriter,rootstr string ,httpcode int,err error){
+	rootstr,_=sjson.Set(rootstr,"code",httpcode)
+	llerr,ok:=err.(*LLError)
+	var errcode int32 = 1
+	var msg string
+	if err!=nil {
+		if (ok) {
+			errcode = llerr.Code()
+			msg = llerr.Message()
+		} else {
+			msg = err.Error()
+		}
+	}
 	switch httpcode {
 	case 200:
-		sjson.Set(rootstr,"status","ok")
+		rootstr,_=sjson.Set(rootstr,"status","ok")
 	case 400:
-		sjson.Set(rootstr,"status","badrequest")
+		rootstr,_=sjson.Set(rootstr,"status","badrequest")
 	case 500:
-		sjson.Set(rootstr,"status","servererror")
+		rootstr,_=sjson.Set(rootstr,"status","servererror")
 	}
 	w.Header().Set("content-type","application/json; charset=utf-8")
 	if(httpcode!=200){
 		w.WriteHeader(httpcode)
-		sjson.Set(rootstr,"errorCode",errcode)
-		sjson.Set(rootstr,"errorMessage",errmsg)
+		rootstr,_=sjson.Set(rootstr,"errorCode",errcode)
+		rootstr,_=sjson.Set(rootstr,"errorMessage",msg)
 	}
 	w.Write([]byte(rootstr))
 }
 
 func HttpLLErrorRespond(w http.ResponseWriter ,httpcode int,err error){
-	json:=simplejson.New()
-	llerr,ok:=err.(*LLError)
-	var errcode int32 = 1
-	var msg string
-	if(ok) {
-		errcode=llerr.Code()
-		msg=llerr.Message()
-	}else{
-		msg=err.Error()
-	}
-	json.Set("code",httpcode)
-	switch httpcode {
-	//200
-	case  http.StatusOK:
-		json.Set("status","ok")
-		//400
-	case  http.StatusBadRequest:
-		json.Set("status","badrequest")
-	case http.StatusForbidden:
-		json.Set("status","forbidden")
-		//500
-	case  http.StatusInternalServerError:
-		json.Set("status","servererror")
-	}
-	w.Header().Set("content-type","application/json; charset=utf-8")
-	if(httpcode!=200){
-		w.WriteHeader(httpcode)
-		json.Set("errorCode",errcode)
-		json.Set("errorMessage",msg)
-	}
-	b,_:=json.Encode()
-	w.Write(b)
-
+	HttpJsonStrRespond(w,"{}",httpcode,err)
 }
