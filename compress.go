f523fa07488e5bd95ad7e6d9e85d7f38ecfd1eac
WSW <shengwei.wan@ceasia.com.cn>
2020-10-12 16:38:01 +0800

    windows only

--- compress.go
+++ compress.go
@@ -65,39 +65,7 @@ func compress(file *os.File, prefix string, zw *zip.Writer) error {
 	return nil
 }
 
-func DeCompress(zipFile, dest string) error {
-	reader, err := zip.OpenReader(zipFile)
-	if err != nil {
-		return err
-	}
-	defer reader.Close()
-	for _, file := range reader.File {
 
-		rc, err := file.Open()
-		if err != nil {
-			return err
-		}
-		defer rc.Close()
-		filename := dest+file.Name
-
-		err = os.MkdirAll(getDir(filename), 0755)
-		if err != nil {
-			return err
-		}
-		w, err := os.Create(filename)
-		if err != nil {
-			return err
-		}
-		defer w.Close()
-		_, err = io.Copy(w, rc)
-		if err != nil {
-			return err
-		}
-		w.Close()
-		rc.Close()
-	}
-	return nil
-}
 func getDir(path string) string {
 	return subString(path, 0, strings.LastIndex(path, "/"))
 }
